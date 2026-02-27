65c5e719d0ab9eda39c4440abc6001da5c9a6f71
Guilherme <guilhermecesar360@gmail.com>
2026-02-27 20:54:15 -0300

    Vamos de início

---
+++ script.js
@@ -0,0 +1,8 @@
+let quantidadeEstoque = '';
+let quantidadeCompra = '';
+
+if (quantidadeCompra <= quantidadeEstoque) {
+    console.log('Compra realizada com sucesso!');
+} else {
+    console.log('Desculpe, não temos estoque suficiente para atender seu pedido.');
+}
