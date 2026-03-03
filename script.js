abd1f8ee5da3825345ea929d944e99e9a3777307
Guilherme César do Nascimento Menezes <163300634+GuilhermeCesar2317@users.noreply.github.com>
2026-03-03 20:38:27 -0300

    Terceiro envio

--- script.js
+++ script.js
@@ -26,4 +26,59 @@ try {
         alert("Erro ao salvar tarefa: " + error.message);
     }
 
-})
+});
+
+function addTaskToUl(task) {
+    const li = document.createElement('li');
+    li.className = task.completed ? "completed" : "";
+    li.innerHTML = `
+    <span>${task.title} - ${task.description}</span>
+    <div>
+    <button onclick="toggleComplete(${task.completed})">✅</button>
+    <button onclick="deleteTask(${task.id})">🗑️</button>
+    </div>
+    `;
+    task.appendChild(li);
+}
+
+async function loadTasks() {
+    try {
+        const res = await fetch(apiUrl);
+        if (res.ok) throw new Error('Erro ao carregar tarefas');
+        const tasks = await res.json();
+        taskList.innerHTML = "";
+        tasks.forEach(addTaskToUl);
+        
+    } catch (error) {
+        alert("Erro ao carregar tarefas: " + error.message);
+    }
+
+    async function toggleComplete(id, completed) {
+        try{
+            await fetch(`${apiUrl}/${id}`), {
+                method: "PUT",
+                headers: {"Content-Type": "application.json"},
+                body: JSON.stringify({completed: !completed})
+            }
+
+        } catch (error) {
+            alert(`Erro ao atualizar tarefa: ${error.message}`)
+        }
+
+        async function deleteTask(id) {
+            try{
+                await fetch(`${apiURL}/${id}`, {
+                    method: "DELETE"
+                });
+
+                loadTasks();
+            } catch (error) {
+                alert(`Erro ao excluir tarefa(s): ${error.message}`);
+
+            }
+        }
+
+        loadTasks();
+        
+    }
+}
